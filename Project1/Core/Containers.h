0e3154b192cda5b8236c230af18fb70b4e044f92
2eelee <yoo.hyuseo.dev@gmail.com>
2026-09-11 18:23:33 +0900

    feat: 소환되는 Actor 위에 uuid 라벨, 패널 내 Show UUID 체크박스 추가, ObjectManager 내에 파괴 예약 시스템 구현, TArray Swap 함수 구현

--- Project1/Core/Containers.h
+++ Project1/Core/Containers.h
@@ -75,6 +75,9 @@ public:
         ArrayData.clear();
     }                    
     void clear() { ArrayData.clear(); }
+
+    // TArray 내부 저장 공간 맞바꿈
+    void Swap(TArray& Other) { ArrayData.swap(Other.ArrayData); }
     
     // Index 접근 조회
     T& operator[](int index)
