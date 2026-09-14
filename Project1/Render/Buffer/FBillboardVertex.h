c999ae4989e86f62a344f09b8dda212fddbbeecb
taehun01 <rlaxogns0910@gmail.com>
2026-09-14 14:40:15 +0900

    feat: FVector2D, quad_vertices, FBillboardVertex 추가

---
+++ Project1/Render/Buffer/FBillboardVertex.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "FVector.h"
+
+struct FBillboardVertex
+{
+	FVector Position;
+	FVector2D UV;
+};
--- Project1/Resources/Geometry/Sphere.h
+++ Project1/Resources/Geometry/Sphere.h
@@ -2749,6 +2749,19 @@ inline FVertexData arrow_vertices[] =
 	{  0.0900f, 0.70f,  0.0000f }
 };
 
+inline TArray<FVertexData> quad_vertices =
+{
+	// Triangle A
+	{ -1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f }, // Bottom-left (red)
+	{ 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f }, // Bottom-right (green)
+	{ -1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f }, // Top-left (blue)
+
+	// Triangle B
+	{ 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f }, // Bottom-right (green)
+	{ 1.0f,  1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f }, // Top-right (red)
+	{ -1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f }, // Top-left (blue)
+};
+
 inline TArray<FVertexData> rectangle_vertices =
 {
     // Triangle A
