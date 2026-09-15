3267e6f38e42ed72b53dde549375b60843a34554
2eelee <yoo.hyuseo.dev@gmail.com>
2026-09-15 20:50:39 +0900

    fix: main.cpp 예외처리 추가

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,7 +9,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// COM 라이브러리 초기화
 	// COM 객체를 STA(COINIT_APARTMENTTHREADED)방식으로 접근하도록 변경. 사유: Microsoft에서 UI 스레드는 STA 사용을 권장
 
-	CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+	// CoInitializeEx 실패할 시 종료
+	if (FAILED(hr)) return 0;
 
 	srand(static_cast<unsigned>(time(nullptr)));
 
