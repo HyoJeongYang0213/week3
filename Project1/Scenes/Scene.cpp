20e55a559be0f9637183146f185c9f3959c8b703
HYOJEONGYANG <jjjjjeong0213@gmail.com>
2026-09-16 19:38:06 +0900

    fix: 기즈모 튀는 버그 수정 #24
    테스트 방법: GameManager::Render() 마지막부분쯔음 Sleep(200) 걸고 오브젝트 하나 소환해서 클릭하시면 됩니다
    
    기존순서 : Update() -> PickUpdate() -> Render()
    이때 Update()에서 기즈모 크기를 계산을 하는데..
    근데 Pickupdate()을 가야 인풋을 받기때문에 오류가 발생한 것 같습니다
    
    바꾼순서 : PickUpdate() -> Update() -> Render()

--- Project1/Scenes/Scene.cpp
+++ Project1/Scenes/Scene.cpp
@@ -25,6 +25,8 @@ Scene::Scene(): FrameBuffer(DEVICE.CreateConstantBuffer(sizeof(FrameConstants)))
 
 void Scene::Update(float DeltaTime)
 {
+	PICK.Update();
+
 	auto& Objects = OBJECT.GUObjectArray;
 	for (size_t i = 0; i < Objects.size(); ++i)
 	{
@@ -33,7 +35,7 @@ void Scene::Update(float DeltaTime)
 			Objects[i]->Update(DeltaTime);
 		}
 	}
-	PICK.Update();
+	
 	Grid.Update(CAMERA.GetLocation());
 
 	// 삭제 예약 된 객체 정리
