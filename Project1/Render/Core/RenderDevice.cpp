55148f17756c82496adb1fcc2430642c15d7a057
taehun01 <rlaxogns0910@gmail.com>
2026-09-16 19:47:09 +0900

    fix: 나만 70프레임 나오던 원인 찾아 제거

--- Project1/Render/Core/RenderDevice.cpp
+++ Project1/Render/Core/RenderDevice.cpp
@@ -147,8 +147,6 @@ GraphicsPipeline RenderDevice::CreateGraphicsPipeline(
 		.FillMode = Rasterizer.FillMode,
 		.CullMode = Rasterizer.CullMode,
 		.FrontCounterClockwise = Rasterizer.bFrontCounterClockWise,
-		.MultisampleEnable = Rasterizer.MultisampleEnable,
-		.AntialiasedLineEnable = Rasterizer.AntialiasedLineEnable,
 	};
 	Device->CreateRasterizerState(&RasterizerDesc, &Result.RasterizerState);
 
